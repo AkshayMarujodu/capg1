9fb2a3455e36f3fc78b0ad8be5598bfa4e422c13
AkshayMarujodu <100128853+AkshayMarujodu@users.noreply.github.com>
2022-02-24 19:47:50 +0530

    Add files via upload

---
+++ countwordsentence.java
@@ -0,0 +1,15 @@
+package countwordsentence;
+
+import java.util.Scanner;
+
+public class countwordsentence {
+
+	public static void main(String[] args) {
+		
+			Scanner sc=new Scanner(System.in);
+			String a=sc.nextLine();
+		System.out.println(a.length());
+
+	}
+
+}
